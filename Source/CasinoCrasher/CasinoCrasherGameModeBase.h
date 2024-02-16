9368c4da6096746e1dfb987569f63077c057bdec
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-16 22:59:52 +0000

    Casino Crasher V1.2 Files

---
+++ Source/CasinoCrasher/CasinoCrasherGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CasinoCrasherGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASINOCRASHER_API ACasinoCrasherGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
